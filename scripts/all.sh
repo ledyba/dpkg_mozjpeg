1cd8ddd72e908de9592364365b94ec278da37535
psi <ryo.hirafuji@link-u.co.jp>
2021-12-25 07:12:21 +0900

    Fix arm64

--- scripts/all.sh
+++ scripts/all.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-set -eu
+set -eux
 
 ## git リポジトリ上の scripts ディレクトリのパスを取得
-scripts_dir=$(readlink -f $(cd $(dirname $0) && pwd))
+ROOT_DIR="$(readlink -f "$(cd "$(dirname "$0")" && cd .. && pwd)")"
 
 ## パッケージのディレクトリ名
-pkg_dir="mozjpeg"
+PKG_DIR="mozjpeg"
+
+## ビルドに必要なパッケージのインストール
+apt-get update
+apt-get install -y --no-install-recommends build-essential debhelper devscripts debmake equivs
+apt-get install -y --no-install-recommends lsb-release git bash
 
 ## ビルド時に必要なパッケージのインストール
-env --chdir="${scripts_dir}/../${pkg_dir}" \
+env --chdir="${ROOT_DIR}/${PKG_DIR}" \
   mk-build-deps --install --remove \
   --tool='apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes' \
   debian/control
 
 ## deb ファイルのビルド
-bash "${scripts_dir}/create_changelog.sh"
-bash "${scripts_dir}/build.sh"
+bash "${ROOT_DIR}/scripts/create_changelog.sh"
+bash "${ROOT_DIR}/scripts/build.sh"
