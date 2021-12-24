5bd3293ded092132df615b19e41de184ad668cc1
psi <ryo.hirafuji@link-u.co.jp>
2021-12-25 08:59:08 +0900

    run apt-update before install

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,9 +3,10 @@
 set -eux
 
 ## git リポジトリ上の root のパスを取得
-scripts_dir=$(readlink -f $(cd $(dirname $(readlink -f $0)) && pwd))
-root_dir=$(cd ${scripts_dir} && cd .. && pwd)
-cd ${root_dir}
+ROOT_DIR="$(readlink -f "$(cd "$(dirname "$(readlink -f "$0")")" && cd .. && pwd)")"
+cd "${ROOT_DIR}"
+
+apt-get update
 
 apt install -y ./artifact/*.deb
 apt show mozjpeg
@@ -18,5 +19,6 @@ djpeg -version
 cjpeg -version 2>&1 | grep -io mozjpeg
 djpeg -version 2>&1 | grep -io mozjpeg
 
+set +e
 ldd $(which cjpeg)
 ldd $(which djpeg)
